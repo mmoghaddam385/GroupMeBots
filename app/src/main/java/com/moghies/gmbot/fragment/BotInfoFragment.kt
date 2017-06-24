b244d54e50f0dd4f006a0e420304e1c480b6f3f4
Michael Moghaddam <m.moghaddam385@outlook.com>
2017-06-24 14:36:27 -0400

    initial add of bot info view in bot view activity

--- app/src/main/java/com/moghies/gmbot/fragment/BotInfoFragment.kt
+++ app/src/main/java/com/moghies/gmbot/fragment/BotInfoFragment.kt
@@ -2,10 +2,14 @@ package com.moghies.gmbot.fragment
 
 
 import android.os.Bundle
+import android.support.design.widget.TextInputLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 import com.moghies.gmbot.R
 import com.moghies.gmbot.db.BotDbContract
@@ -14,13 +18,73 @@ import com.moghies.gmbot.db.BotDbContract
 /**
  * A simple [Fragment] subclass.
  */
-class BotInfoFragment(bot: BotDbContract.BotsTable.BotEntry) : Fragment() {
+class BotInfoFragment(val bot: BotDbContract.BotsTable.BotEntry) : Fragment() {
 
+    lateinit private var txtBotId: EditText
+    lateinit private var txtBotIdWrapper: TextInputLayout
+
+    lateinit private var txtBotName: EditText
+    lateinit private var txtBotNameWrapper: TextInputLayout
+
+    lateinit private var txtBotGroup: EditText
+    lateinit private var txtBotGroupWrapper: TextInputLayout
+
+    private var isInEditMode = false
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_bot_info, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_bot_info, container, false)
+
+        txtBotId = view.findViewById(R.id.txtBotId) as EditText
+        txtBotIdWrapper = view.findViewById(R.id.txtBotIdWrapper) as TextInputLayout
+
+        txtBotName = view.findViewById(R.id.txtBotName) as EditText
+        txtBotNameWrapper = view.findViewById(R.id.txtBotNameWrapper) as TextInputLayout
+
+        txtBotGroup = view.findViewById(R.id.txtBotGroup) as EditText
+        txtBotGroupWrapper = view.findViewById(R.id.txtBotGroupWrapper) as TextInputLayout
+
+        txtBotId.setText(bot.id, TextView.BufferType.EDITABLE)
+        txtBotName.setText(bot.name, TextView.BufferType.EDITABLE)
+        txtBotGroup.setText(bot.groupName, TextView.BufferType.EDITABLE)
+
+        view.findViewById(R.id.btnEdit).setOnClickListener { view -> onEditClicked(view as Button) }
+        view.findViewById(R.id.btnRemove).setOnClickListener { onRemoveClicked() }
+
+        return view
     }
 
-}// Required empty public constructor
+    private fun onEditClicked(btn: Button) {
+        if (isInEditMode) {
+            saveChanges()
+            btn.setText(R.string.edit_bot)
+        } else {
+            btn.setText(R.string.save_changes)
+            enterEditMode()
+        }
+
+        isInEditMode = !isInEditMode
+    }
+
+    private fun onRemoveClicked() {
+
+    }
+
+    private fun saveChanges() {
+        txtBotName.isEnabled = false
+        txtBotGroup.isEnabled = false
+
+        txtBotIdWrapper.clearFocus()
+        txtBotNameWrapper.clearFocus()
+        txtBotGroupWrapper.clearFocus()
+
+        // TODO: actually save it
+    }
+
+    private fun enterEditMode() {
+        txtBotName.isEnabled = true
+        txtBotGroup.isEnabled = true
+    }
+
+}
