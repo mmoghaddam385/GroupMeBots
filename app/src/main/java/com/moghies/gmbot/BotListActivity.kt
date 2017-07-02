5628fa2453084f23166bec0e980f635395d853a9
Michael Moghaddam <m.moghaddam385@outlook.com>
2017-07-02 13:11:50 -0400

    remove toolbar menus and view activity's parent

--- app/src/main/java/com/moghies/gmbot/BotListActivity.kt
+++ app/src/main/java/com/moghies/gmbot/BotListActivity.kt
@@ -118,22 +118,4 @@ class BotListActivity : AppCompatActivity() {
         startActivityForResult(intent, 0)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_bot_list, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
 }
